a3f98ad9aedbaacc4750e62a068cb935315f3db6
Sean Murphy <sean@iamseanmurphy.com>
2026-08-04 22:22:39 +0000

    2.6.1

--- designsetgo/build/blocks/form-builder/view.asset.php
+++ designsetgo/build/blocks/form-builder/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'db81d1ebb626e0229fbb');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '9f5e48c96580dfbab79d');
--- designsetgo/build/utils/sticky-header.asset.php
+++ designsetgo/build/utils/sticky-header.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '739ff019ae68db42ae34');
+<?php return array('dependencies' => array(), 'version' => 'd2dd25e283e46f56fb62');
